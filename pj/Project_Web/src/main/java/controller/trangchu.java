9189382bfd9063c4234e606384cfd7ed9bf456be
Thành Luân <Thnh Lun@DESKTOP-DEBAO9O>
2025-12-26 16:36:47 +0700

    Commit

--- pj/Project_Web/src/main/java/controller/trangchu.java
+++ pj/Project_Web/src/main/java/controller/trangchu.java
@@ -1,5 +1,53 @@
 package controller;
 
-public class trangchu {
+import java.io.IOException;
+
+import dao.DAOType;
+import entity.Cart;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class trangchu
+ */
+public class trangchu extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public trangchu() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		if (session.getAttribute("GioHang") == null)
+			session.setAttribute("GioHang", new Cart());
+		if (session.getAttribute("meunType") == null)
+			session.setAttribute("menuType", new DAOType().getAll());
+
+		request.getServletContext().getRequestDispatcher("/trangchu.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
